c9cd53aedf3a7d90fa04332fb13fd08fe3c65f1b
JUYERINA <j35148@gmail.com>
2023-07-08 15:32:49 +0900

    game 챌린지 추가

---
+++ 3.challenge/game.ts
@@ -0,0 +1,44 @@
+interface Position {
+  x: number;
+  y: number;
+}
+
+const position: Position = {
+  x: 0,
+  y: 0,
+};
+
+type Direction = "up" | "down" | "left" | "right";
+
+function move(direction: Direction): Position {
+  switch (direction) {
+    case "up":
+      position.y++;
+      break;
+    case "down":
+      position.y--;
+      break;
+    case "left":
+      position.x--;
+      break;
+    case "right":
+      position.x++;
+      break;
+    default:
+      throw new Error("unknown direction");
+  }
+  return position;
+}
+
+/**
+ * Let's make a game 🕹
+ */
+console.log(position); // { x: 0, y: 0}
+move("up");
+console.log(position); // { x: 0, y: 1}
+move("down");
+console.log(position); // { x: 0, y: 0}
+move("left");
+console.log(position); // { x: -1, y: 0}
+move("right");
+console.log(position); // { x: 0, y: 0}
