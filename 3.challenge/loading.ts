ee2b422054a02bfd685576f7ba9fa31e1b491ef5
orongee22 <j35148@gmail.com>
2023-07-08 15:42:29 +0900

    loading 챌린지 추가

---
+++ 3.challenge/loading.ts
@@ -0,0 +1,38 @@
+{
+  /**
+   * Print Loading State
+   */
+  type LoadingState = {
+    state: "loading";
+  };
+
+  type SuccessState = {
+    state: "success";
+    response: {
+      body: string;
+    };
+  };
+
+  type FailState = {
+    state: "fail";
+    reason: string;
+  };
+
+  type ResourceLoadState = LoadingState | SuccessState | FailState;
+
+  function printLoginState(state: ResourceLoadState): void {
+    if (state.state === "loading") {
+      console.log(`👀 ${state.state}`);
+    } else if (state.state === "success") {
+      console.log(`😃 ${state.response.body}`);
+    } else if (state.state === "fail") {
+      console.log(`😱 ${state.reason}`);
+    } else {
+      throw new Error("error");
+    }
+  }
+
+  printLoginState({ state: "loading" }); // 👀 loading...
+  printLoginState({ state: "success", response: { body: "loaded" } }); // 😃 loaded
+  printLoginState({ state: "fail", reason: "no network" }); // 😱 no network
+}
