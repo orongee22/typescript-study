1787173fa96d1fbd722f585f18f1c899c64df645
orongee22 <j35148@gmail.com>
2023-08-21 11:57:39 +0900

    4.10 getter / setter

---
+++ 4.oop/4.getter,setter.ts
@@ -0,0 +1,77 @@
+{
+	type CoffeeCup = {
+		shots: number;
+		hasMilk: boolean;
+	}
+	class CoffeeMaker {
+		// BEANS_GRAMM_PER_SHOT: number = 7; // 멤버 변수
+		private static BEANS_GRAMM_PER_SHOT: number = 7;
+		private coffeeBeans: number = 0;
+
+		constructor(coffeeBeans: number) {
+			this.coffeeBeans = coffeeBeans;
+		}
+
+		static makeMachine(coffeeBeans: number): CoffeeMaker {
+			return new CoffeeMaker(coffeeBeans);
+		}
+
+        fillCoffeeBeans(beans: number) {
+            if(beans< 0) {
+                throw new Error("value for beans should be greater than 0");
+            }
+
+            this.coffeeBeans = beans;
+        }
+		makeCoffee(shots: number): CoffeeCup {
+			if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT) {
+				throw new Error("Not enough coffee beans");
+			}
+
+			this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT;
+			return {
+				shots, 
+				hasMilk: false
+			}
+				
+		}
+
+		addCoffeeBeans(amount: number) {
+			if(amount < 0) {
+				throw new Error("커피콩을 넣어주세여")
+			}
+			this.coffeeBeans = amount;
+		}
+	}
+ 
+	const maker = new CoffeeMaker(10);
+    maker.fillCoffeeBeans(10);
+
+    class User {
+        // private firstName: string;
+        // private lastName: string;
+
+        get fullName(): string {
+            return `${this._firstName} ${this._lastName}`;
+        }
+
+        set firstName(name: string) {
+            this._firstName = name;
+        }
+
+        set lastName(name: string) {
+            this._lastName = name;
+        }
+
+        // set 
+
+        // constructor(firstName: string, lastName: string) {
+        constructor(private _firstName: string, private _lastName: string) {}
+    }
+
+    const user = new User('Steve', 'Jobs');
+    console.log(user.fullName)
+
+    user.firstName = 'dpfdfjs';
+    console.log(user.fullName);
+}
